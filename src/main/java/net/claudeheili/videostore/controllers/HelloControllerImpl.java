191e3b0b3e8fcb51a4cbc049499abe13d0d633b8
Claude HEILI <informatique.cheili@gmail.com>
2018-08-30 10:52:48 +0200

    Added Login logic

--- src/main/java/net/claudeheili/videostore/controllers/HelloControllerImpl.java
+++ src/main/java/net/claudeheili/videostore/controllers/HelloControllerImpl.java
@@ -1,17 +1,20 @@
 package net.claudeheili.videostore.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.claudeheili.videostore.interfaces.HelloController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
 @RestController
 public class HelloControllerImpl implements HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
 
 }
