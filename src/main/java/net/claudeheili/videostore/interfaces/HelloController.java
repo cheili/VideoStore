7c212c96020383206762bc5afa06f56def3ca7bc
Claude HEILI <informatique.cheili@gmail.com>
2018-08-30 19:26:59 +0200

    Commit

--- src/main/java/net/claudeheili/videostore/interfaces/HelloController.java
+++
@@ -1,6 +0,0 @@
-package net.claudeheili.videostore.interfaces;
-
-public interface HelloController {
-
-	String index() throws Exception;
-}
--- src/main/java/net/claudeheili/videostore/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.claudeheili.videostore.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import net.claudeheili.videostore.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
