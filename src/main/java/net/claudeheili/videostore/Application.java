08c518306da1e98ade485ce168113e4f426f64cf
Claude HEILI <informatique.cheili@gmail.com>
2018-08-31 13:13:42 +0200

    Addded -- Athentication

--- src/main/java/net/claudeheili/videostore/Application.java
+++ src/main/java/net/claudeheili/videostore/Application.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
